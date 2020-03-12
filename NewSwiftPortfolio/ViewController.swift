50e9917efa38339671dfaa38355f7262a1a51c07
Ashika Palacharla <ashika.palacharla@gmail.com>
2020-03-12 08:21:33 -0700

    Initial Commit

---
+++ NewSwiftPortfolio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewSwiftPortfolio
+//
+//  Created by ashi on 3/12/20.
+//  Copyright © 2020 Ashika Palacharla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
